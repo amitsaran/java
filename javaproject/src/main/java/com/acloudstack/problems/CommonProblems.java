dd40740ebc9126c59cd67111f95e1e4b19c11ea3
Amit Saran <amitsaran@gmail.com>
2021-12-31 13:45:08 -0500

    minor changes

--- javaproject/src/main/java/com/acloudstack/problems/CommonProblems.java
+++ javaproject/src/main/java/com/acloudstack/problems/CommonProblems.java
@@ -2,12 +2,9 @@ package com.acloudstack.problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class CommonProblems {
 
